ab0997921590025569cd724d5f2e7618ffc2c8b4
Masterchef365 <duncan.freeman1@gmail.com>
2021-05-23 18:48:34 -0700

    Some tests

---
+++ notes.md
@@ -0,0 +1,19 @@
+Case 1:
+A not toward B
+B in evidence
+B toward C
+
+Case 2:
+A toward B
+B in evidence
+B toward C
+
+Case 2:
+A not toward B
+B in evidence
+B not toward C
+
+Case 3:
+A toward B
+B not in evidence
+B not toward C
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,35 @@
 use anyhow::{Context, Result};
 use std::collections::{HashMap, HashSet};
-use std::path::Path;
 
 type Node = char;
 
 fn main() -> Result<()> {
+    // Parse args
     let mut args = std::env::args().skip(1);
     let graph_path = args.next().context("Requires graph path")?;
-    let graph: Graph = read_graph(graph_path)?;
 
-    let start_node: Node = args.next().context("Requires first query")?.chars().next().unwrap();
-    let end_node: Node = args.next().context("Requires second query")?.chars().next().unwrap();
+    let start_node: Node = args
+        .next()
+        .context("Requires first query")?
+        .chars()
+        .next()
+        .unwrap();
+    let end_node: Node = args
+        .next()
+        .context("Requires second query")?
+        .chars()
+        .next()
+        .unwrap();
     let evidence: Vec<Node> = args.map(|s| s.chars().next().unwrap()).collect();
 
+    // Read file
+    let string = std::fs::read_to_string(graph_path)?;
+    let graph = read_graph(&string)?;
+
+    // Calculate
     let d_separated = d_separated(&graph, start_node, end_node, &evidence)?;
 
+    // Output
     print!("{} and {} are ", start_node, end_node);
     if d_separated {
         println!("separated");
@@ -28,11 +43,13 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-pub fn read_graph(path: impl AsRef<Path>) -> Result<Graph> {
-    let string = std::fs::read_to_string(path)?;
+pub fn read_graph(s: &str) -> Result<Graph> {
     let mut graph: Graph = HashMap::new();
 
-    for (line_idx, line) in string.lines().enumerate() {
+    for (line_idx, line) in s.lines().enumerate() {
+        // Ignore whitespace before and after
+        let line = line.trim();
+
         // Skip empty lines
         if line.is_empty() {
             continue;
@@ -78,7 +95,12 @@ pub struct Edge {
     pub end: Node,
 }
 
-pub fn d_separated(graph: &Graph, start_node: Node, end_node: Node, evidence: &[Node]) -> Result<bool> {
+pub fn d_separated(
+    graph: &Graph,
+    start_node: Node,
+    end_node: Node,
+    evidence: &[Node],
+) -> Result<bool> {
     let mut visited: HashSet<Node> = HashSet::new();
     let mut lifo: Vec<(Option<bool>, Node)> = vec![(None, start_node)];
     let evidence_set: HashSet<Node> = evidence.iter().copied().collect();
@@ -94,7 +116,7 @@ pub fn d_separated(graph: &Graph, start_node: Node, end_node: Node, evidence: &[
             let in_evidence = evidence_set.contains(&node);
             let blocked = match last_was_toward {
                 None => false,
-                Some(last_was_toward) => blocked(last_was_toward, in_evidence, edge.toward),
+                Some(last_was_toward) => is_blocked(last_was_toward, in_evidence, edge.toward),
             };
 
             if !blocked {
@@ -105,16 +127,64 @@ pub fn d_separated(graph: &Graph, start_node: Node, end_node: Node, evidence: &[
             }
         }
     }
-    
+
     Ok(true)
 }
 
-fn blocked(last_was_toward: bool, in_evidence: bool, next_is_toward: bool) -> bool {
+fn is_blocked(last_was_toward: bool, in_evidence: bool, next_is_toward: bool) -> bool {
     match (last_was_toward, in_evidence, next_is_toward) {
         (false, true, true) => true,
         (true, true, true) => true,
         (false, true, false) => true,
         (true, false, false) => true,
         _ => false,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_assn_graph() {
+        let graph = get_graph();
+        assert!(!both_ways(&graph, 'A', 'B', &[]).unwrap());
+        assert!(!both_ways(&graph, 'A', 'E', &[]).unwrap());
+        assert!(both_ways(&graph, 'A', 'E', &['B']).unwrap());
+        assert!(!both_ways(&graph, 'A', 'C', &[]).unwrap());
+        assert!(!both_ways(&graph, 'A', 'F', &[]).unwrap());
+        assert!(both_ways(&graph, 'A', 'F', &['C']).unwrap());
+        assert!(!both_ways(&graph, 'A', 'G', &['C']).unwrap());
+        assert!(both_ways(&graph, 'A', 'G', &['C', 'B']).unwrap());
+        assert!(!both_ways(&graph, 'D', 'H', &['C', 'B']).unwrap());
+        assert!(both_ways(&graph, 'D', 'H', &['C', 'B', 'F']).unwrap());
+        assert!(!both_ways(&graph, 'C', 'G', &[]).unwrap());
+        assert!(!both_ways(&graph, 'C', 'G', &['F']).unwrap());
+        assert!(both_ways(&graph, 'C', 'G', &['F', 'A']).unwrap());
+    }
+
+    fn both_ways(
+        graph: &Graph,
+        a: Node,
+        b: Node,
+        evidence: &[Node],
+    ) -> Result<bool> {
+        let ab = d_separated(graph, a, b, evidence)?;
+        let ba = d_separated(graph, b, a, evidence)?;
+        assert!(ab == ba, "Gives different results in opposite directions!");
+        Ok(ab)
+    }
+
+    fn get_graph() -> Graph {
+        let string = "
+            A -> B
+            A -> C
+            C -> F
+            D -> F
+            B -> E
+            E -> G
+            F -> G
+            F -> H
+        ";
+        read_graph(string).unwrap()
     }
 }
